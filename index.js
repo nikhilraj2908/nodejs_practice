556db98e87b6d47abe59f4549c4a7cc2ae1d8575
nikhilraj2908 <your_email@example.com>
2025-01-17 18:59:48 +0530

    feat:server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http=require("http")
+const fs=require("fs")
 
 const server=http.createServer((req,res)=>{
     console.log("hello from server")
-    res.end("res end from server")
+    const log=`${Date.now()} ${req.url} new req received\n`;
+    fs.appendFile("userlog.txt",log,(err,result)=>{
+        res.end("res end from server")
+    })
+ 
 })
 server.listen(8000,()=>{console.log("server started")})
